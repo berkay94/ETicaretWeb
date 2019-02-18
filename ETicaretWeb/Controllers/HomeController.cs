c8cd6e68e8833061e4e467ad3fa661321edd2d5d
semvs <semvs@ayvansaray.edu.tr>
2019-02-18 14:46:23 +0300

    edit

--- ETicaretWeb/Controllers/HomeController.cs
+++ ETicaretWeb/Controllers/HomeController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ETicaretWeb.Models;
 namespace ETicaretWeb.Controllers
 {
     public class HomeController : Controller
     {
+        ETicaretWebEntities db = new ETicaretWebEntities();
         public ActionResult Index()
         {
+            ViewBag.KategoriListesi = db.Kategori.ToList();
+            ViewBag.SonUrunler = db.Urunler.OrderByDescending(a => a.UrunId).Skip(0).Take(12).ToList();
             return View();
         }
 
+        public ActionResult Kategori(int id)
+        {
+            ViewBag.KategoriListesi = db.Kategori.ToList();
+            ViewBag.Kategori = db.Kategori.Find(id);
+            return View(db.Urunler.Where(a=>a.RefKategoriID==id).OrderBy(a=>a.UrunAdi).ToList());
+        }
+
+        public ActionResult Urun(int id)
+        {
+            ViewBag.KategoriListesi = db.Kategori.ToList();
+            return View(db.Urunler.Find(id));
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
